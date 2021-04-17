98dd9275a9f07592d0aa058b9b2151989cace920
Dylan Hardison <dylan@hardison.net>
2021-04-17 16:08:25 -0400

    first really good version
    supports a few options the plain text clipboard content, including one
    where links will be replaced by their urls.

--- mdcopy/main.swift
+++ mdcopy/main.swift
@@ -7,34 +7,110 @@
 
 import Foundation
 import class AppKit.NSPasteboard
+import ArgumentParser
+import Maaku
 
+let TransientType = NSPasteboard.PasteboardType("org.nspasteboard.TransientType")
 
-import func cmark.cmark_gfm_markdown_to_html
-import var cmark.CMARK_OPT_SMART
-import var cmark.CMARK_OPT_DEFAULT
-import var cmark.CMARK_OPT_GITHUB_PRE_LANG
+enum TextFormat: String, ExpressibleByArgument {
+    case raw, text, markdown
+}
 
-let TransientType = NSPasteboard.PasteboardType.init("org.nspasteboard.TransientType")
+struct mdcopy: ParsableCommand {
+    @Flag(name: .shortAndLong, help: "Enable smart quotes")
+    var smartQuotes: Bool = false
+    
+    
+    @Option(name: .shortAndLong, help: "Format of clipboard plain text version")
+    var format: TextFormat = .raw
+    
+    @Option(name: .shortAndLong, help: "Text width")
+    var width: Int32 = 80
+    
+    @Argument(help: "input file")
+    var inputFile: String?
 
-let pb = NSPasteboard.general
-let text = FileHandle.standardInput.availableData
-let html = markdown(text)
-
-pb.prepareForNewContents(with: NSPasteboard.ContentsOptions.init())
-pb.clearContents()
-pb.setData(text, forType: NSPasteboard.PasteboardType.string)
-pb.setData(html ?? text, forType: NSPasteboard.PasteboardType.html)
-pb.setData(Data.init(), forType: TransientType)
-
-
-func markdown(_ text: Data) -> Data? {
-    let opts = CMARK_OPT_DEFAULT | CMARK_OPT_GITHUB_PRE_LANG | CMARK_OPT_SMART
-    let text_bytes: [Int8] = [UInt8](text).map {Int8(bitPattern: $0)}
-    guard let html_bytes = cmark_gfm_markdown_to_html(text_bytes, text.count, opts) else {
-        return nil
+    mutating func run() throws {
+        let markdown = try readInput()
+        setClipboard(html: try renderHtml(markdown), text: try renderText(markdown))
     }
     
-    let html = String(cString: html_bytes).data(using: String.Encoding.utf8)
-    html_bytes.deallocate()
-    return html
+    func markdownOptions() -> CMDocumentOption {
+        let options: CMDocumentOption = [.default, .unsafe, .preLang, .strikethroughDoubleTilde]
+        if smartQuotes {
+            return options.union( .smart )
+        }
+        else {
+            return options
+        }
+    }
+    
+    func renderHtml(_ text: Data) throws -> String {
+        let doc = try CMDocument(data: text, options: markdownOptions())
+        return try doc.renderHtml()
+    }
+    
+    func renderText(_ text: Data) throws -> String {
+        let doc = try CMDocument(data: text, options: markdownOptions())
+        
+        switch format {
+        case .raw:
+            return String(data: text, encoding: .utf8)!
+        case .markdown:
+            return try doc.renderCommonMark(width: width)
+        case .text:
+            var currentLink: URL?
+            try doc.node.iterator?.enumerate {
+                (node: CMNode, event: CMEventType) -> Bool in
+                switch (event, node.type) {
+                case (.enter, .link):
+                    currentLink = node.linkUrl
+                case (.enter, .text):
+                    if let link = currentLink {
+                        try node.setLiteral(link.absoluteString)
+                    }
+                default:
+                    break
+                }
+                return false
+            }
+            return try doc.renderPlainText(width: width)
+        }
+    }
+    
+    func setClipboard(html: String, text: String) {
+        let pb = NSPasteboard.general
+        pb.prepareForNewContents(with: NSPasteboard.ContentsOptions())
+        pb.clearContents()
+        pb.setString("<meta charset=\"UTF-8\">" + html, forType: .html)
+        pb.setString(text, forType: .string)
+        pb.setString("", forType: TransientType)
+    }
+    
+    func readInput() throws -> Data {
+        if let file = inputFile {
+            let fileUrl = URL(fileURLWithPath: file)
+            return slurp(try FileHandle(forReadingFrom: fileUrl))
+        }
+        else {
+            return slurp(FileHandle.standardInput)
+        }
+    }
+    
+    func slurp(_ fh: FileHandle) -> Data {
+        var chunk: Data
+        var input = Data()
+        repeat {
+            chunk = fh.availableData
+            input.append(chunk)
+        } while chunk.count > 0
+        return input
+    }
 }
+
+mdcopy.main()
+
+
+
+
+
