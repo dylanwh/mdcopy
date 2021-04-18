d4bf5ea8ef95059223ff89a24663941d51e81a14
Dylan Hardison <dylan@hardison.net>
2021-04-18 10:48:39 -0400

    add final set of features I need for my workflows
    - new option -t/--transient marks pasteboard content as transient (previously this
      was the default)
    - new option -i/--input removes need for "echo ... | " construction,
      useful in alfred workflows
    - option help descriptions improved

--- mdcopy/main.swift
+++ mdcopy/main.swift
@@ -5,116 +5,122 @@
 //  Created by Dylan Hardison on 2021-04-12.
 //
 
-import Foundation
-import class AppKit.NSPasteboard
 import ArgumentParser
+import Foundation
 import Maaku
+
+import class AppKit.NSPasteboard
 
 let TransientType = NSPasteboard.PasteboardType("org.nspasteboard.TransientType")
 
 struct mdcopy: ParsableCommand {
 
-    enum TextFormat: String, ExpressibleByArgument {
-        case raw, text, markdown
+  enum TextFormat: String, ExpressibleByArgument {
+    case raw, text, markdown
+  }
+
+  @Flag(name: .shortAndLong, help: "Enable smart quotes")
+  var smartQuotes: Bool = false
+
+  @Flag(name: .shortAndLong, help: "Mark pasteboard with transient hint")
+  var transient: Bool = false
+
+  @Option(name: .shortAndLong, help: "Text format of plain text pasteboard variant")
+  var format: TextFormat = .raw
+
+  @Option(name: .shortAndLong, help: "Text width")
+  var width: Int32 = 80
+
+  @Option(name: .shortAndLong, help: "Input string")
+  var input: String?
+
+  @Argument(help: "input file")
+  var inputFile: String?
+
+  mutating func run() throws {
+    let markdown = try readInput()
+    setClipboard(html: try renderHtml(markdown), text: try renderText(markdown))
+  }
+
+  func parse(markdown: Data) throws -> CMDocument {
+    let extensions: CMExtensionOption = .all
+    var options: CMDocumentOption = [.default, .unsafe, .preLang, .strikethroughDoubleTilde]
+    if smartQuotes {
+      options.insert(.smart)
     }
-    
-    @Flag(name: .shortAndLong, help: "Enable smart quotes")
-    var smartQuotes: Bool = false
-    
-    @Option(name: .shortAndLong, help: "Format of clipboard plain text version")
-    var format: TextFormat = .raw
-    
-    @Option(name: .shortAndLong, help: "Text width")
-    var width: Int32 = 80
-    
-    @Argument(help: "input file")
-    var inputFile: String?
-    
-    mutating func run() throws {
-        let markdown = try readInput()
-        setClipboard(html: try renderHtml(markdown), text: try renderText(markdown))
-    }
-    
-    func parse(markdown: Data) throws -> CMDocument {
-        let extensions: CMExtensionOption = .all
-        var options: CMDocumentOption = [.default, .unsafe, .preLang, .strikethroughDoubleTilde]
-        if smartQuotes {
-            options.insert(.smart)
+
+    return try CMDocument(
+      text: String(data: markdown, encoding: .utf8)!,
+      options: options,
+      extensions: extensions)
+  }
+
+  func renderHtml(_ text: Data) throws -> String {
+    let doc = try parse(markdown: text)
+    return try doc.renderHtml()
+  }
+
+  func renderText(_ text: Data) throws -> String {
+    let doc = try parse(markdown: text)
+
+    switch format {
+    case .raw:
+      return String(data: text, encoding: .utf8)!
+    case .markdown:
+      return try doc.renderCommonMark(width: width)
+    case .text:
+      var currentLink: URL?
+      try doc.node.iterator?.enumerate {
+        (node: CMNode, event: CMEventType) -> Bool in
+        switch (event, node.type) {
+        case (.enter, .link):
+          currentLink = node.linkUrl
+        case (.enter, .text):
+          if let link = currentLink {
+            try node.setLiteral(link.absoluteString)
+          }
+        default:
+          break
         }
-        
-        return try CMDocument(
-            text: String(data: markdown, encoding: .utf8)!,
-            options: options,
-            extensions: extensions)
+        return false
+      }
+      return try doc.renderPlainText(width: width)
     }
-    
-    func renderHtml(_ text: Data) throws -> String {
-        let doc = try parse(markdown: text)
-        return try doc.renderHtml()
+  }
+
+  func setClipboard(html: String, text: String) {
+    let pb = NSPasteboard.general
+    pb.prepareForNewContents(with: NSPasteboard.ContentsOptions())
+    pb.clearContents()
+    /* without the meta, this will be interpreted as latin1! */
+    pb.setString("<meta charset=\"UTF-8\">" + html, forType: .html)
+    pb.setString(text, forType: .string)
+    if transient {
+      pb.setString("", forType: TransientType)
     }
-    
-    func renderText(_ text: Data) throws -> String {
-        let doc = try parse(markdown: text)
-        
-        switch format {
-        case .raw:
-            return String(data: text, encoding: .utf8)!
-        case .markdown:
-            return try doc.renderCommonMark(width: width)
-        case .text:
-            var currentLink: URL?
-            try doc.node.iterator?.enumerate {
-                (node: CMNode, event: CMEventType) -> Bool in
-                switch (event, node.type) {
-                case (.enter, .link):
-                    currentLink = node.linkUrl
-                case (.enter, .text):
-                    if let link = currentLink {
-                        try node.setLiteral(link.absoluteString)
-                    }
-                default:
-                    break
-                }
-                return false
-            }
-            return try doc.renderPlainText(width: width)
-        }
+  }
+
+  func readInput() throws -> Data {
+    if let file = inputFile {
+      let fileUrl = URL(fileURLWithPath: file)
+      let fileHandle = try FileHandle(forReadingFrom: fileUrl)
+      return slurp(fileHandle)
+    } else if let text = input {
+      return text.data(using: .utf8, allowLossyConversion: true)!
+    } else {
+      return slurp(FileHandle.standardInput)
     }
-    
-    func setClipboard(html: String, text: String) {
-        let pb = NSPasteboard.general
-        pb.prepareForNewContents(with: NSPasteboard.ContentsOptions())
-        pb.clearContents()
-        /* without the meta, this will be interpreted as latin1! */
-        pb.setString("<meta charset=\"UTF-8\">" + html, forType: .html)
-        pb.setString(text, forType: .string)
-        pb.setString("", forType: TransientType)
-    }
-    
-    func readInput() throws -> Data {
-        if let file = inputFile {
-            let fileUrl = URL(fileURLWithPath: file)
-            return slurp(try FileHandle(forReadingFrom: fileUrl))
-        }
-        else {
-            return slurp(FileHandle.standardInput)
-        }
-    }
-    
-    func slurp(_ fh: FileHandle) -> Data {
-        var chunk: Data
-        var input = Data()
-        repeat {
-            chunk = fh.availableData
-            input.append(chunk)
-        } while chunk.count > 0
-        return input
-    }
+  }
+
+  func slurp(_ fh: FileHandle) -> Data {
+    var chunk: Data
+    var input = Data()
+    repeat {
+      chunk = fh.availableData
+      input.append(chunk)
+    } while chunk.count > 0
+    return input
+  }
 }
 
 mdcopy.main()
-
-
-
-
-
